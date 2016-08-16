46bcf029358a98db1bf05ad21bf67d3c14c51c49
Alexandre Thebaldi <ahlechandre@gmail.com>
2016-08-16 19:08:04 -0300

    active routes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,25 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const Layout = ({ children }) =>
-  <main>
-    <header>
-      <p>Header</p>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/items">Items</Link></li>
-      </ul>
-    </header>
-    <section>{children}</section>
-    <footer>
-      <p>Footer</p>
-    </footer>
-  </main>
-  ;
+const Layout = ({ children, params, location }, context) => {
+  const { router } = context;
+  console.log(router.isActive('/'));
+  const styleActive = {
+    fontWeight: 'bold',
+  };
+  return (
+    <main>
+      <header>
+        <p>Header</p>
+        <ul>
+          <li>
+            <Link
+              to="/"
+              activeStyle={styleActive}
+              activeClassName="is-active"
+              onlyActiveOnIndex
+            >Home</Link>
+          </li>
+          <li>
+            <Link
+              activeStyle={styleActive}
+              to="/about"
+              activeClassName="is-active"
+            >About</Link>
+          </li>
+          <li>
+            <Link
+              activeStyle={styleActive}
+              to="/items"
+              activeClassName="is-active"
+            >Items</Link></li>
+        </ul>
+      </header>
+      <section>{children}</section>
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </main>
+  );
+};
+
+Layout.contextTypes = {
+  router: React.PropTypes.object,
+};
 
 Layout.propTypes = {
   children: React.PropTypes.node,
+  params: React.PropTypes.object,
+  location: React.PropTypes.object,
+  history: React.PropTypes.object,
 };
 
 export default Layout;
